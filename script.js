904a4eaa087f4b69d8ba577d75fd714a7a089210
Jelena <81640389+Jelena1912@users.noreply.github.com>
2021-12-13 12:02:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ const apiUrl= `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count
 // Check if all images were loaded
 function imageLoaded(){
     console.log('image loaded');
-    imageLoaded++;
-    console.log(imageLoaded);
-    if(imageLoaded === totalImages){
+    imagesLoaded++;
+    console.log(imagesLoaded);
+    if(imagesLoaded === totalImages){
         ready = true;
         console.log('ready = ', ready);
     }
@@ -40,16 +40,16 @@ function displayPhotos(){
     console.log('total images', totalImages);
     // Run function for each object in photosArray
     photosArray.forEach((photo) => {
-        // Cfreate <a> to link to Unsplash
+        // Create <a> to link to Unsplash
         const item= document.createElement('a');
         // item.setAttribute('href', photo.links.html);
         // item.setAttribute('target', '_blank');
-        setAttributes(item, {
+        setAttributes(item, {                   //item- element za funkciju setAttributes, href: key, target-key, 
         href: photo.links.html,
         target: '_blank', 
         });
-        // Create <img> for foto
-        const img= document.createElement('img');
+        // Create <img> for pfoto
+        const img = document.createElement('img');
         // img.setAttribute('src', photo.urls.regular);
         // img.setAttribute('alt', photo.alt_description);
         // img.setAttribute('title', photo.alt_description);
@@ -59,12 +59,11 @@ function displayPhotos(){
             title: photo.alt_description,
         });
 
-// Event Listener, check when each is finished loading
-img.addEventListener('load', imageLoaded);
+        // Event Listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
 
 
-
-        // pUT <IMG> INSIde <a>, then put both inside imageContainer Element
+        //Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
     });
@@ -76,7 +75,7 @@ img.addEventListener('load', imageLoaded);
 // Get photos from unsplash API
 async function getPhotos(){
     try{
-        const response=await fetch(apiUrl);
+        const response = await fetch(apiUrl);
         photosArray = await response.json();
         displayPhotos();
 
